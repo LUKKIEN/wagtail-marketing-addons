923cfbe5cae71180a330a2f2e5569a701ab777b4
Janneke Janssen <j.janssen@lukkien.com>
2019-03-14 20:24:14 +0100

    Remove unused import

--- src/wagtail_marketing/wagtail_hooks.py
+++ src/wagtail_marketing/wagtail_hooks.py
@@ -1,4 +1,3 @@
-from django.apps import apps
 from django.template.defaultfilters import truncatechars
 from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
