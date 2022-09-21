f64c7698e62be4bc14d1c1a91421708e8a8ceb51
Wesley van Lee <w.vanlee@lukkien.com>
2022-09-21 17:47:49 +0200

    Add official support up to wagtail 4

--- tests/settings.py
+++ tests/settings.py
@@ -4,7 +4,7 @@ from wagtail import VERSION as WAGTAIL_VERSION
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DATABASE_ENGINE' , 'django.db.backends.postgresql_psycopg2'),
+        'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.postgresql_psycopg2'),
         'NAME': os.getenv('DATABASE_NAME', 'wagtail_marketing'),
         'HOST': os.getenv('DATABASE_HOST', ''),
         'USER': os.getenv('DATABASE_USER', ''),
